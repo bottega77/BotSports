80d91572f09d6d053f18a28d66688cd6e7f49227
bottega77 <157405832+bottega77@users.noreply.github.com>
2024-02-29 16:39:22 -0300

    Add files via upload

---
+++ BotSports/camisa.js
@@ -0,0 +1,182 @@
+let camisaJson = [
+  {
+      id: 1,
+      name: 'Fiorentina',
+      img: 'images/camisa_1-transformed.png',
+      price: [900.00, 900.00, 900.00],
+      sizes: [
+          'equena',
+          'édia', 
+          'rande'
+      ],
+      description: 'Temporada 1998/99'
+    },
+    {
+      id: 2,
+      name: 'AFC Richmond',
+      img: 'images/camisa_2-transformed.webp',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022/23'
+    },
+    {
+      id: 3,
+      name: 'Grêmio',
+      img: 'images/camisa_3-transformed.png',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2023/24'
+    },
+    {
+      id: 4,
+      name: 'Vasco da Gama',
+      img: 'images/camisa_11-transformed.webp',
+      price: [230.00, 230.00, 230.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022'
+    },
+    {
+      id: 5,
+      name: 'Milan',
+      img: 'images/camisa_5-transformed.webp',
+      price: [300.00, 300.00, 300.00],
+      sizes: [
+        'equena',
+        'édia',
+        'rande'
+      ],
+      description: 'Temporada 2013/14'
+    },
+    {
+      id: 6,
+      name: 'Venezia',
+      img: 'images/camisa_6-transformed.png',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022'
+    },
+    {
+      id: 7,
+      name: 'Internazionale',
+      img: 'images/camisa_7-transformed.png',
+      price: [320.00, 320.00, 320.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2003/04 '
+    },
+    {
+      id: 8,
+      name: 'Norwich',
+      img: 'images/camisa_8-transformed.png',
+      price: [260.00, 260.00, 260.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022/23'
+    },
+    {
+      id: 9,
+      name: 'Inter Miami',
+      img: 'images/camisa_12-transformed.webp',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2023'
+    },
+    {
+      id: 10,
+      name: 'Alemanha',
+      img: 'images/camisa_10-transformed.png',
+      price: [370.00, 370.00, 370.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 1995'
+    },
+    {
+      id: 11,
+      name: 'Nacional',
+      img: 'images/camisa_9-transformed.png',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022/23'
+    },
+    {
+      id: 12,
+      name: 'Santos',
+      img: 'images/camisa_12-transformed.png',
+      price: [300.00, 300.00, 300.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2012'
+    },
+    {
+      id: 13,
+      name: 'Brasil',
+      img: 'images/camisa_13-transformed.png',
+      price: [275.00, 275.00, 275.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022/24'
+    },
+    {
+      id: 15,
+      name: 'Ajax',
+      img: 'images/camisa_14-transformed.png',
+      price: [250.00, 250.00, 250.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2021/22'
+    },
+    {
+      id: 15,
+      name: 'Barcelona',
+      img: 'images/camisa_15-transformed.png',
+      price: [265.00, 265.00, 265.00],
+      sizes: [
+          'equena',
+          'édia',
+          'rande'
+      ],
+      description: 'Temporada 2022/23'
+    }
+];
